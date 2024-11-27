8ff9eed334fc2226799870e84335c8a0b5e8ada8
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-27 07:35:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ API_HASH = "abe760b5d6b33e15c676577d6ae4a06a"  # Replace with your API Hash
 BOT_TOKEN = "7902514308:AAGRWf0i1sN0hxgvVh75AlHNvcVpJ4j07HY"  # Replace with your bot token
 
 # MongoDB Configuration
-MONGO_URI = "mongodb+srv://Teamsanki:Teamsanki@cluster0.jxme6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB URI (update if needed)
-DB_NAME = "Teamsanki"  # MongoDB database name
+MONGO_URI = "mongodb://localhost:27017"  # MongoDB URI (update if needed)
+DB_NAME = "music_bot"  # MongoDB database name
 
 # Initialize Pyrogram Client
 app = Client("music_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
